c9764b61601448ad35af13d279ea692739f10a7e
David A. Eilertsen <david.eilertsen@remerge.io>
2021-07-14 10:49:29 +0200

    new version with java

--- Makefile
+++ Makefile
@@ -6,9 +6,16 @@ all:
 	go install google.golang.org/protobuf/cmd/protoc-gen-go
 	go install github.com/srikrsna/protoc-gen-gotag
 	go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto
+	mkdir proto/java
+	mkdir proto/go
 	protoc -I /usr/local/include -I proto \
-		--go_out=paths=source_relative:./proto \
+		--java_out=./proto/java \
+		--go_out=paths=source_relative:./proto/go \
 		--go-vtproto_out=$(GOPATH)/src --plugin protoc-gen-go-vtproto="${GOBIN}/protoc-gen-go-vtproto" \
 		--go-vtproto_opt=features=marshal+unmarshal+size \
 		proto/*.proto
 	protoc -I /usr/local/include -I proto/ --gotag_out=outdir="$(GOPATH)/src":$(GOPATH)/src proto/*.proto
+
+clean:
+	rm -fr proto/go
+	rm -fr proto/java
--- okey/main.go
+++ okey/main.go
@@ -2,66 +2,44 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"time"
 
-	"github.com/davidae/proto-definition/proto"
-	"github.com/xitongsys/parquet-go/parquet"
-	"github.com/xitongsys/parquet-go/writer"
+	greeting_proto "github.com/davidae/proto-definition/proto"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
-
-type LOL struct {
-	From        string  `parquet:"name=from, type=BYTE_ARRAY"`
-	LastUpdated int64   `parquet:"name=last_updated, type=INT64, convertedtype=TIMESTAMP_MILLIS"`
-	To          *string `parquet:"name=to, type=BYTE_ARRAY"`
-}
 
 func main() {
 	from := "David"
 	to := "Lia"
-	greet := proto.Greeting{
+	greet := greeting_proto.Greeting{
 		From:                from,
 		To:                  &to,
 		LastUpdated:         timestamppb.New(time.Now()),
 		PublisherCategories: []int32{1, 2, 3},
 	}
 
-	greetSimple := proto.GreetingSimple{
-		From:                from,
-		To:                  &to,
-		LastUpdated:         time.Now().Unix(),
-		PublisherCategories: []int32{1, 2, 3},
-	}
-
-	// lol := LOL{
-	// 	From: "david", To: &to, LastUpdated: time.Now().Unix(),
+	// greetSimple := greeting_proto.GreetingSimple{
+	// 	From:                from,
+	// 	To:                  &to,
+	// 	LastUpdated:         time.Now().Unix(),
+	// 	PublisherCategories: []int32{1, 2, 3},
 	// }
 
-	fmt.Println(greet.String())
-
-	var err error
-	w, err := os.Create("output/flat.parquet")
+	data, err := greet.MarshalVT()
 	if err != nil {
 		panic(err)
 	}
 
-	//write
-	pw, err := writer.NewParquetWriterFromWriter(w, new(proto.GreetingSimple), 4)
-	if err != nil {
+	greeterVT := &greeting_proto.Greeting{}
+	if err := greeterVT.UnmarshalVT(data); err != nil {
 		panic(err)
 	}
-	pw.RowGroupSize = 128 * 1024 * 1024 //128M
-	pw.CompressionType = parquet.CompressionCodec_SNAPPY
-	if err = pw.Write(greetSimple); err != nil {
-		panic(err)
-	}
+	fmt.Printf("getting with UnmarshalVT: %s\n", greeterVT.String())
 
-	if err := pw.WriteStop(); err != nil {
+	greeterBasic := &greeting_proto.Greeting{}
+	if err := proto.Unmarshal(data, greeterBasic); err != nil {
 		panic(err)
 	}
-
-	if err := w.Close(); err != nil {
-		panic(err)
-	}
+	fmt.Printf("getting with basic: %s\n", greeterVT.String())
 }
