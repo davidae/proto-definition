bcd6773f71158d0552d9843c07de5f89e2a8c713
David A. Eilertsen <david.eilertsen@remerge.io>
2021-07-19 13:06:15 +0200

    vtproto

--- go.mod
+++ go.mod
@@ -3,12 +3,7 @@ module github.com/davidae/proto-definition
 go 1.16
 
 require (
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/planetscale/vtprotobuf v0.2.0 // indirect
-	github.com/spf13/afero v1.6.0 // indirect
-	github.com/srikrsna/protoc-gen-gotag v0.6.1 // indirect
-	github.com/xitongsys/parquet-go v1.6.0
-	golang.org/x/text v0.3.6 // indirect
+	github.com/gogo/protobuf v1.3.2
+	github.com/golang/protobuf v1.5.2
 	google.golang.org/protobuf v1.27.1
 )
--- okey/main.go
+++ okey/main.go
@@ -4,22 +4,23 @@ import (
 	"fmt"
 	"time"
 
-	greeting_proto "github.com/davidae/proto-definition/proto"
+	go_proto "github.com/davidae/proto-definition/proto/go"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func main() {
 	from := "David"
-	to := "Lia"
-	greet := greeting_proto.Greeting{
+	to := ""
+	greet := go_proto.Greeting{
 		From:                from,
 		To:                  &to,
 		LastUpdated:         timestamppb.New(time.Now()),
 		PublisherCategories: []int32{1, 2, 3},
 	}
 
-	// greetSimple := greeting_proto.GreetingSimple{
+	// greetSimple := go_proto.GreetingSimple{
 	// 	From:                from,
 	// 	To:                  &to,
 	// 	LastUpdated:         time.Now().Unix(),
@@ -31,15 +32,29 @@ func main() {
 		panic(err)
 	}
 
-	greeterVT := &greeting_proto.Greeting{}
+	greeterVT := &go_proto.Greeting{}
 	if err := greeterVT.UnmarshalVT(data); err != nil {
 		panic(err)
 	}
 	fmt.Printf("getting with UnmarshalVT: %s\n", greeterVT.String())
 
-	greeterBasic := &greeting_proto.Greeting{}
+	greeterBasic := &go_proto.Greeting{}
 	if err := proto.Unmarshal(data, greeterBasic); err != nil {
 		panic(err)
 	}
 	fmt.Printf("getting with basic: %s\n", greeterVT.String())
+
+	// wrap := gogo_proto.GreetingWrapp{
+	// 	Baz: &gogo_proto.GreetingWrapp_BazValue{BazValue: 1},
+	// }
+	// fmt.Printf("wrap: %#v\n", wrap)
+
+	reflect := greet.ProtoReflect()
+	fmt.Println("protoreflect start")
+	reflect.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+		fmt.Printf("jsonName: %s, cardinality: %s, presence: %t\n", fd.JSONName(), fd.Cardinality().GoString(), fd.HasPresence())
+		fmt.Printf("  isValid? %t, value: %#v\n", v.IsValid(), v.Interface())
+		return true
+	})
+	fmt.Println("protoreflect end")
 }
